bedc13257edf7964138d35e1eba0ca60b0ba2592
ericksilva232323 <erick14silva14@gmail.com>
2023-09-25 11:12:16 -0300

    aual13

---
+++ 1des/FPOO/Aula 13/EX1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL," ");
+	FILE *arquivo, *saida;
+	char result[28];
+	int dados[10] = {0}, i, soma = 0;
+	
+	arquivo = fopen ("dados.txt", "r");
+	if(arquivo == NULL)
+		printf("Erro, não foi possivel abrir o arquivo\n");
+		
+	else{
+		while((fgets(result, 80, arquivo)) != NULL){
+			
+			dados[0] = atoi(strtok(result," "));
+			soma += dados[0];
+		for (i = 1; i < 10; i++){
+			dados[i] = atoi(strtok(NULL," "));
+			soma += dados[i];
+		}
+		
+	printf("valores: %d", dados[0]);
+	for(i = 1; 1 < 10; i++){
+		printf(" - %d", dados[i]);
+		}
+		printf(" = soma dos valores: %d", soma);
+	}	
+}
+		
+}
