6dfe61874ea309726602a04f0e23b9eaf9f70333
ericksilva232323 <erick14silva14@gmail.com>
2023-10-16 11:42:28 -0300

    up

--- 1des/SOP/Aula6/sistema/script.js
+++ 1des/SOP/Aula6/sistema/script.js
@@ -12,9 +12,25 @@ function inss(salario){
     
 }
 function irrf(salario){
-    return 0;
+    if(salario < 1320.01)
+        return salario * 7.5 / 100;
+    else if(salario < 2571.30)
+        return salario * 15 / 100;
+    else if(salario < 3856.95)
+        return salario * 22.5 / 100;
+    else if(salario < 7507.49)
+        return salario * 27.5 / 100;
+    else
+        return 1051.05;
 }
+let salario = 1000;
+let inss = inss(salario);
+let salarioBase = salario - inss;
+let irrf = irrf(salario);
+let salarioLiquido = salarioBase - irrf;
 
-console.log(inss(1000));
-
-
+console.log("salario: " + salario);
+console.log("INSS: " + inss);
+console.log("Salario Base:" + salarioBase);
+console.log("IRRF: " + irrf);
+console.log("Salario Liquido:" + salarioLiquido);
