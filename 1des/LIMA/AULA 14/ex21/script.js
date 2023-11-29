9d508aa25f5d7a89abb2837b04d4b3c19335d549
ericksilva232323 <erick14silva14@gmail.com>
2023-11-29 11:36:55 -0300

    29/11/23-11:20

--- 1des/LIMA/AULA 14/ex21/script.js
+++ 1des/LIMA/AULA 14/ex21/script.js
@@ -1,5 +1,5 @@
-function contar() {
-    let saida = document.getElementById('saida')
+function contar21(){
+    let saida = document.getElementById('saida21')
 
     saida.innerHTML += `<h2>Contando de 1 até 10</h2>`
 
@@ -9,4 +9,51 @@ function contar() {
         cont ++ // Corresponde a cont = cont + 1
     }
     saida.innerHTML += ` &#x1F3C1;`
+}
+
+function contar22() {
+    let saida = document.getElementById('saida22')
+
+    saida.innerHTML += `<h2>Contando de 1 até 10</h2>`
+
+    let cont = 1
+    while (cont <= 10) {
+        if (cont % 2 == 0){
+            saida.innerHTML += `<mark><strong> ${cont} </strong></mark>&#x1F449;` 
+        }else{
+            saida.innerHTML += ` ${cont} &#x1F449;` 
+        }
+        cont ++
+    }
+    saida.innerHTML += ` &#x1F3C1`
+}
+
+function contar23(){
+    let saida = document.getElementById('saida23')
+
+    saida.innerHTML += `<h2>Contando de 1 até 10</h2>`
+
+    let cont = 1
+    while(cont <= 10){
+        if(cont % 2 == 1){
+            saida.innerHTML += `<mark> <strong> ${cont} </strong></mark>&#x1F449;`
+        }else{
+            saida.innerHTML += ` ${cont} &#x1F449;`
+        }
+        cont ++
+    }
+    saida.innerHTML += ` &#x1F3C1;`
+}
+
+function contar24(){
+        let saida = document.getElementById('saida24')
+    
+        saida.innerHTML += `<h2>Contando de 10 até 1</h2>`
+    
+        let cont = 1
+        while (cont <= 10) {
+            saida.innerHTML += ` ${cont} &#x1F449;`
+            cont ++ // Corresponde a cont = cont + 1
+        }
+        saida.innerHTML += ` &#x1F3C1;`
 }
