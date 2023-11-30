6d271e422008040cbb3b9c6f648a0df98724e489
ericksilva232323 <erick14silva14@gmail.com>
2023-11-30 11:39:59 -0300

    30/11/23-11:39

--- 1des/LIMA/AULA 14/ex21/script.js
+++ 1des/LIMA/AULA 14/ex21/script.js
@@ -50,10 +50,9 @@ function contar24(){
     
         saida.innerHTML += `<h2>Contando de 10 até 1</h2>`
     
-        let cont = 1
-        while (cont <= 10) {
+        let cont = i
+        for (let i = 0; i <= 10; i++) {
             saida.innerHTML += ` ${cont} &#x1F449;`
-            cont ++ // Corresponde a cont = cont + 1
         }
         saida.innerHTML += ` &#x1F3C1;`
 }
